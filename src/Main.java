6b4181aa2ab72250eff1a7363b3f5c7310c95d8a
User <nazarovtolya2@gmail.com>
2023-03-05 21:08:12 +0500

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static int checkYear(int year) {
@@ -38,6 +39,23 @@ public class Main {
             System.out.println("Установите облегченную версию для Android по ссылке.");
         } else {
             System.out.println("Ваша операционная система не поддерживается.");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        calculateDelivery(425);
+        }
+
+    public static void calculateDelivery (int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            System.out.println("Доставка до клиента занимает 1 день");
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Доставка до клиента занимает 2 дня");
+        } else if (deliveryDistance <=100) {
+            System.out.println("Доставка до клиента занимает 3 дня");
+        } else {
+            System.out.println("Нет доставки");
         }
     }
 
