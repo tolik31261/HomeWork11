fa327a8bce4b4aea1e87979714f6c77b0a5de6e6
User <nazarovtolya2@gmail.com>
2023-03-05 20:56:43 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int checkYear(int year) {
@@ -16,6 +17,28 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         int year = checkYear(2021);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int year = 2025;
+        installSystem(2022);
+    }
+
+    public static void installSystem(int clientDeviceYear) {
+        int clientOs = 0;
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear > currentYear && clientOs == 0) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке.");
+        } else if (clientDeviceYear < currentYear && clientOs == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientDeviceYear > currentYear && clientOs == 1) {
+            System.out.println("Установите обычную версию для Android по ссылке.");
+        } else if (clientDeviceYear < currentYear && clientOs == 1) {
+            System.out.println("Установите облегченную версию для Android по ссылке.");
+        } else {
+            System.out.println("Ваша операционная система не поддерживается.");
+        }
     }
 
 }
